377f23e7117f11f7859e206268822ce5ecb86d50
LEESOOYE <sooye7@naver.com>
2022-03-17 17:28:35 +0900

    naver 검색, 네비게이터, 프로필, 광고 배너, 뉴스 스탠드 추가

---
+++ js/press.js
@@ -0,0 +1,17 @@
+function clickPress(){
+    var pressTable=document.querySelectorAll("#pressTable");
+    
+
+    for(let i=0;i<pressTable.length;i++){
+        pressTable[i].getAttribute("td").addEventListener("mouseover",function(){
+
+        });
+        
+    }
+
+
+
+    
+
+
+}
